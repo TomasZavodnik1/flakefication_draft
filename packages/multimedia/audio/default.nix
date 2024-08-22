6a483919d6aadea2382acaaa9de3c628aef4dfa0
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-08-22 21:51:46 +0400

    sync

--- packages/multimedia/audio/default.nix
+++ packages/multimedia/audio/default.nix
@@ -14,23 +14,19 @@ in
   with lib; {
       environment.systemPackages = [
         #main gstreamer lib
-        gst_all_1.gstreamer
+        pkgs.gst_all_1.gstreamer
         # Common plugins like "filesrc" to combine within e.g. gst-launch
-        gst_all_1.gst-plugins-base
+        pkgs.gst_all_1.gst-plugins-base
         # Specialized plugins separated by quality
-        gst_all_1.gst-plugins-good
-        gst_all_1.gst-plugins-bad
-        gst_all_1.gst-plugins-ugly
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-bad
+        pkgs.gst_all_1.gst-plugins-ugly
         # Plugins to reuse ffmpeg to play almost every video format
-        gst_all_1.gst-libav
+        pkgs.gst_all_1.gst-libav
         # Support the Video Audio (Hardware) Acceleration API
-        gst_all_1.gst-vaapi
-        #tools to install opus enc
-        libopusenc    
-        #tools to install opus dec
-        opus_decoder
+        pkgs.gst_all_1.gst-vaapi
         # Basic alsa utils needed for sound testing and gstreamer correct function
-        alsa-utils
+        pkgs.alsa-utils
       ];
   }
 
