6ddd923d62653fa928cea88dafb6569dfa9174d7
Tomas Zavodnik <tomas.zavodnik@tii.ae>
2024-08-22 21:54:34 +0400

    sync

--- packages/default.nix
+++ packages/default.nix
@@ -12,7 +12,20 @@
 in
   with lib; {
       environment.systemPackages = with pkgs; [
-          #morse micro driver
-          gstreamer-pkg
+        #main gstreamer lib
+        pkgs.gst_all_1.gstreamer
+        # Common plugins like "filesrc" to combine within e.g. gst-launch
+        pkgs.gst_all_1.gst-plugins-base
+        # Specialized plugins separated by quality
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-bad
+        pkgs.gst_all_1.gst-plugins-ugly
+        # Plugins to reuse ffmpeg to play almost every video format
+        pkgs.gst_all_1.gst-libav
+        # Support the Video Audio (Hardware) Acceleration API
+        pkgs.gst_all_1.gst-vaapi
+        # Basic alsa utils needed for sound testing and gstreamer correct function
+        pkgs.alsa-utils
+
       ];
 }
